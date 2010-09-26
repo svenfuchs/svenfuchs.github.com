a94284f4a722aa3917d157fd39e90d29250589ab
Sven Fuchs <svenfuchs@artweb-design.de>
2010-09-27 00:57:16 +0200

    fix footer

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100415000003) do
+ActiveRecord::Schema.define(:version => 20100904224918) do
 
   create_table "accounts", :force => true do |t|
+  end
+
+  create_table "cache_taggings", :force => true do |t|
+    t.string "url"
+    t.string "tag"
   end
 
   create_table "contents", :force => true do |t|
